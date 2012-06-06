22338956dc473554e45540ff88461ca0544fcd03
Bradley Temple <bradley@bradley-online.com>
2012-06-05 23:00:07 -0400

    editing routes

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,4 +2,22 @@ class ForumsController < ApplicationController
   def index
     @forums = Forum.all
   end
+  
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
